d621b926c7bcfc86629b5c22a625525d64c40fbd
osamamousa204 <osamamousa204@gmail.com>
2023-09-14 17:57:02 +0300

    adding new logo

--- src/views/pages/Home/index.js
+++ src/views/pages/Home/index.js
@@ -1,3 +1,9 @@
-const Home = () => <div className="home">Home</div>;
+import logo from 'assets/images/jawharat-alhayaa.jpg';
+
+const Home = () => (
+    <div style={{ padding: '20px' }} className="home">
+        <img src={logo} style={{ width: '800px' }} alt="Jawharat Alhayaa" width="100" />
+    </div>
+);
 
 export default Home;
